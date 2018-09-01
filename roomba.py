83891816ff739b571f87db06d55a82f419970266
sbansal6 <sbansal6@gelib-4c-35.ews.illinois.edu>
2018-09-01 18:07:51 -0500

    more changes to roomba, having trouble with obs and free_map because they only go till the border of their territory

--- roomba.py
+++ roomba.py
@@ -33,10 +33,11 @@ class PolicyGen:
         """
         self.random = np.random
         self.free_map = free_map
-        self.heading_right = [True] * len(agent_list) #: Attr to track directions.
-        self.heading_left = [True] * len(agent_list)
-        self.heading_north = [True] * len(agent_list)
-        self.heading_south = [True] * len(agent_list)
+        # initialize all the directions with False
+        self.heading_right = [False] * len(agent_list) #: Attr to track directions.
+        # self.heading_left = [False] * len(agent_list)
+        self.heading_up = [True] * len(agent_list)
+        # self.heading_down = [False] * len(agent_list)
 
     def gen_action(self, agent_list, observation, free_map=None):
         """Action generation method.
@@ -60,6 +61,13 @@ class PolicyGen:
         if free_map is not None: self.free_map = free_map
 
         for idx, agent in enumerate(agent_list):
+            # First choose a random direction to go into
+            starting_action = self.random.randint(0, 3)
+            # Initializing the direction based on the starting_action
+            # if starting_action == 1:
+            #     self.heading_up[idx] = True
+            # elif starting_action == 2:
+            #     self.heading_right[idx] = True
             a = self.roomba(agent, idx, observation)
             action_out.append(a)
 
@@ -76,21 +84,98 @@ class PolicyGen:
 
         x,y = agent.get_loc()
         action = 0
+        # 1 -> heading_up
+        # 2 -> heading_right
+        # 3 -> heading_down
+        # 4 -> heading_left
+
+        # heading up and down
+        # and
+        #     self.free_map[x][y-1] == self.free_map[x][y])
+
+        # if self.heading_up[idx]:
+        #     if y > 0:
+        #         if self.free_map[x][y-1] == self.free_map[]
+
+        if (self.heading_up[idx] and y > 0 and
+            obs[x][y-1] == self.free_map[x][y]):
+            action = 1
+        elif (not self.heading_up[idx] and y < len(self.free_map[0])-1 and
+            obs[x][y+1] == self.free_map[x][y]):
+            action = 3
+        else:
+            if (self.heading_up[idx] and y > 0 and
+                obs[x][y-1] != self.free_map[x][y]):
+                action = 3
+            elif (not self.heading_up[idx] and y < len(self.free_map[0])-1 and
+                obs[x][y+1] != self.free_map[x][y]):
+                action = 1
+            else:
+                self.heading_up[idx] = not self.heading_up[idx]
+
+        return action
+
+        # in case it has reached the border
+        # elif (y < 0 or y >= len(self.free_map[0])):
+        #     self.heading_up[index] = not self.heading_up[index]
+        #
+        # else:
+        #     if (self.heading_right[index] and
+        #         x < len(self.free_map)-1 and
+        #         obs[x+1][y] == self.free_map[x][y]):
+        #         action = 2
+        #     elif (not self.heading_right[index] and
+        #         x > 0 and
+        #         obs[x-1][y] == self.free_map[x][y]):
+        #         action = 4
+
+
+
+
+        # if (y <= 0 or y > len(self.free_map[0])):
+        #     self.heading_up[index] = not self.heading_up[index]
+        #
+        # if (x <= 0 or x >= len(self.free_map)-1):
+        #     self.heading_right[index] = not self.heading_right[index]
+        #
+        # if action == 1 and self.heading_up[index] == False:
+        #     action = 3
+        #
+        # if action == 2 and self.heading_right[index] == False:
+        #     action = 4
+
+        # if action == 1:
+        #     self.heading_up[index] = True
+        #
+        # if action == 2:
+        #     self.heading_right[index] = True
+        #     self.heading_up[index] = False
+        #
+        # # Now cases to set the direction of the player
+        # if self.heading_right[index] == True:
+        #     action = 2
+        # else:
+        #     action = 4
+        #
+        # if self.heading_up[index] == True:
+        #     action = 1
+        # else:
+        #     action = 3
+
+        # first make sure if there is an obstacle
+        # if (x <=0 or x >= len(self.free_map)-1 or
+        #     )
         # rand_action = random.choice(0, 5)
         # if obstacle at left then choose any direction except left
 
-        action = self.random.randint(0, 5)
-
-        if (x>0 and obs[x-1][y] != self.free_map[x][y]):
-            action = int(random.choice([0,1,2,3]))
-
-        elif (x+1 < len(self.free_map) and obs[x+1][y] != self.free_map[x][y]):
-            action = int(random.choice([0,1,4,3]))
-
-        elif (y > len(self.free_map) and obs[x][y-1] != self.free_map[x][y]):
-            action = int(random.choice([0,2,4,3]))
-
-        elif (y < len(self.free_map) - 1 and obs[x][y+1] != self.free_map[x][y]):
-            action = int(random.choice([0,1,4,2]))
-
-        return action
+        # if (x>0 and obs[x-1][y] != self.free_map[x][y]):
+        #     action = int(random.choice([0,1,2,3]))
+        #
+        # elif (x+1 < len(self.free_map) and obs[x+1][y] != self.free_map[x][y]):
+        #     action = int(random.choice([0,1,4,3]))
+        #
+        # elif (y > len(self.free_map) and obs[x][y-1] != self.free_map[x][y]):
+        #     action = int(random.choice([0,2,4,3]))
+        #
+        # elif (y < len(self.free_map) - 1 and obs[x][y+1] != self.free_map[x][y]):
+        #     action = int(random.choice([0,1,4,2]))
